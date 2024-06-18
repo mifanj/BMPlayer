7aa39e0f99e075cd0fe262e7b56a162b18606de9
mifanj <jy2211@qq.com>
2024-06-18 12:17:36 +0800

    feat: add NVActivityIndicatorView dependency

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1"))
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView.git", .upToNextMajor(from: "5.2.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -21,7 +22,8 @@ let package = Package(
         .target(
             name: "BMPlayer",
             dependencies: [
-                .product(name: "SnapKit", package: "SnapKit")
+                .product(name: "SnapKit", package: "SnapKit"),
+                .product(name: "NVActivityIndicatorView", package: "NVActivityIndicatorView"),
             ],
             path: "Source"),
         .testTarget(
