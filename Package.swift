a24887782cd5fe27c499d168a75a829ed574e3d9
mifanj <jy2211@qq.com>
2024-06-18 11:57:39 +0800

    feat: reorder property

--- Package.swift
+++ Package.swift
@@ -20,8 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BMPlayer",
-            path: "Source",
-            dependencies: []),
+            dependencies: [],
+            path: "Source"),
         .testTarget(
             name: "BMPlayerTests",
             dependencies: ["BMPlayer"]),
