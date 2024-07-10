a03342a3d99ec2a9eb1b8b9345c72d1cc2937de1
mifanj <jy2211@qq.com>
2024-07-10 14:36:22 +0800

    feat: update new slider thumb rect

--- Source/BMTimeSlider.swift
+++ Source/BMTimeSlider.swift
@@ -18,8 +18,9 @@ public class BMTimeSlider: UISlider {
     
     override open func thumbRect(forBounds bounds: CGRect, trackRect rect: CGRect, value: Float) -> CGRect {
         let rect = super.thumbRect(forBounds: bounds, trackRect: rect, value: value)
-        let newx = rect.origin.x - 10
-        let newRect = CGRect(x: newx, y: 0, width: 30, height: 30)
+        let newx = rect.origin.x - 11.5
+        let newy = rect.origin.y - 11.5
+        let newRect = CGRect(x: newx, y: newy, width: 30, height: 30)
         return newRect
     }
 }
