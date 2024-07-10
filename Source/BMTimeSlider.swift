13a17a0fecb3cd3a677d68101461cb428e9f3194
mifanj <jy2211@qq.com>
2024-07-10 14:25:13 +0800

    feat: change time slider size

--- Source/BMTimeSlider.swift
+++ Source/BMTimeSlider.swift
@@ -10,9 +10,8 @@ import UIKit
 
 public class BMTimeSlider: UISlider {
     override open func trackRect(forBounds bounds: CGRect) -> CGRect {
-        let trackHeight: CGFloat = 2
-        let position = CGPoint(x: 0, y: 14)
-        let customBounds = CGRect(origin: position, size: CGSize(width: bounds.size.width, height: trackHeight))
+        let trackHeight: CGFloat = 3
+        let customBounds = CGRect(origin: .zero, size: CGSize(width: bounds.size.width, height: trackHeight))
         super.trackRect(forBounds: customBounds)
         return customBounds
     }
